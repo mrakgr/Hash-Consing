557d5ddcc7d24c189ba68832c8e95959108123f2
Marko Grdinić <mrakgr@gmail.com>
2017-09-20 18:11:38 +0200

    Started this project.
    The way create and clear functions are implemented in the Ocaml repo make me extremely suspicious of them, so much that I opened an issue on the relevant repo.
    
    I only did the first few functions now and had to implement the WeakArray class. I've started pretty late in the day and have no more energy left.
    
    I'll aim to port at least half tomorrow and finish the rest by Friday.

--- Hash Consing/hashcons.fsx
+++ Hash Consing/hashcons.fsx
@@ -12,12 +12,15 @@ let gentag =
     fun () -> i <- i+1; i
 
 open System.Runtime.InteropServices
-type WeakArray<'a>(ar) =
+type WeakArray<'a>(ar) = // Partly based on: https://www.codeproject.com/Articles/43042/WeakReferences-GCHandles-and-WeakArrays
     new i = WeakArray(Array.init i (fun _ -> GCHandle.Alloc(null,GCHandleType.Weak)))
     member __.Length = ar.Length
     member __.Item 
-        with set(i,v: 'a) = ar.[i].Target <- v
-        and get(i) = ar.[i].Target :?> 'a
+        with set i (v: 'a) = ar.[i].Target <- v
+        and get(i) = 
+            match ar.[i].Target with
+            | null -> None
+            | v -> Some (v :?> 'a)
         
     override __.Finalize() = 
         let rec loop i =
@@ -26,23 +29,41 @@ type WeakArray<'a>(ar) =
                 if x.IsAllocated then x.Free(); loop (i+1)
         loop 0
 
-//type Table<'a> =
-//    {
-//    mutable table : WeakArray<HashConsed<'a>> []
-//    mutable totsize : int // sum of the bucket sizes
-//    mutable limit : int // max ratio totsize/table length
-//    }
-//
-//let create sz =
-//    let sz = max 7 sz // There is no need to check for the upper bound since it is System.Int32.MaxValue
-//    {
-//    table = Array.init sz <| fun _ -> WeakArray 0
-//    totsize = 0
-//    limit = 3
-//    }
-//
-//let clear t =
-//    for i=0 to Array.length t.table - 1 do t.table.[i] <- WeakArray 0
-//    t.totsize <- 0
-//    t.limit <- 3
-//
+let weak_length (x: WeakArray<_>) = x.Length
+
+type Table<'a> =
+    {
+    mutable table : WeakArray<HashConsed<'a>> []
+    mutable totsize : int // sum of the bucket sizes
+    mutable limit : int // max ratio totsize/table length
+    }
+
+let create sz =
+    let sz = max 7 sz // There is no need to check for the upper bound since it is System.Int32.MaxValue
+    {
+    table = Array.init sz <| fun _ -> WeakArray 0
+    totsize = 0
+    limit = 3
+    }
+
+let clear t =
+    for i=0 to Array.length t.table - 1 do t.table.[i] <- WeakArray 0
+    t.totsize <- 0
+    t.limit <- 3
+
+let foldr f t init =
+    let rec fold_bucket i b accu =
+        if i >= weak_length b then accu else
+            match b.[i] with
+            | None -> fold_bucket (i+1) b accu
+            | Some v -> fold_bucket (i+1) b (f v accu)
+    Array.foldBack (fold_bucket 0) t.table init
+        
+let iter f t =
+    let rec iter_bucket i b =
+        if i >= weak_length b then () else
+            match b.[i] with
+            | None -> iter_bucket (i+1) b
+            | Some v -> f v; iter_bucket (i+1) b
+    Array.iter (iter_bucket 0) t.table
+
